82fb0f26422a8b3fa58b9654ff60f769be23b0a4
NiteshNigam2702 <niteshnigam03@gmail.com>
2025-07-06 12:57:54 +0530

    Moved contents from parent folder to root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,7 +119,7 @@ export default function Home() {
   return (
     <Layout title="Home">
       <section className="text-center py-20">
-        <h1 className="text-4xl font-bold">Hi, I'm Nitesh Nigam👋</h1>
+        <h1 className="text-4xl font-bold">Hi, I&apos;m Nitesh 👋 </h1>
         <p className="mt-4 text-lg text-gray-600">SDE | Blogger | Creator</p>
       </section>
     </Layout>
